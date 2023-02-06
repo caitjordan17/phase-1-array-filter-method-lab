17790a489d3ece19a7241e215d1c80552c3b8552
Caitlin Jordan <jordan.cait17@gmail.com>
2023-02-05 18:40:05 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+  function findMatching(drivers, string){
+    const filteredDrivers = drivers.filter(function(driver){
+      return string.toLowerCase() === driver.toLowerCase()
+    });
+    return filteredDrivers;
+  }
+
+  function fuzzyMatch(drivers, string){
+    let x = string.length
+    const filteredDrivers = drivers.filter(
+      function(driver){
+    return string.toLowerCase() === driver.toLowerCase().substring(0,x)
+  });
+  return filteredDrivers;
+}
+
+function matchName(drivers, string) {
+    const filteredDrivers = drivers.filter(
+        function(driver){
+        return string.toLowerCase() === driver.name.toLowerCase()
+      });
+      return filteredDrivers;
+    }
